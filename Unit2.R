4cfee146beaa83dbc207080f695d35a894184a2f
Jiajun Liu <jliu@cityworks.com>
2016-06-24 11:35:56 -0600

    Finished Elantra Sales

--- Unit2.R
+++ Unit2.R
@@ -142,3 +142,95 @@ RMSE_flu2 = sqrt(SSE_flu2/nrow(fluTest))
 RMSE_flu2
 
 
+
+
+
+####################Forecasting Elantra Sales 
+elantra = read.csv("elantra.csv")
+head(elantra)
+str(elantra)
+elantraTrain = subset(elantra, Year <= 2012)
+str(elantraTrain)
+elantraTest = subset(elantra,Year > 2012)
+
+salesModel1 = lm(ElantraSales~Unemployment +CPI_all+CPI_energy+Queries,data=elantraTrain)
+summary(salesModel1)
+
+salesModel2 = lm(ElantraSales~Month + Unemployment +CPI_all+CPI_energy+Queries,data=elantraTrain)
+summary(salesModel2)
+
+110.69 * 2
+110.69 * 4
+
+elantraTrain$MonthFactor = as.factor(elantraTrain$Month)
+elantraTest$MonthFactor = as.factor(elantraTest$Month)
+salesModel3 = lm(ElantraSales~MonthFactor + Unemployment +CPI_all+CPI_energy+Queries,data=elantraTrain)
+summary(salesModel3)
+
+pairs(elantraTrain)
+cor(elantraTrain[c("Unemployment","Month","Queries","CPI_energy","CPI_all")])
+
+salesModel4 = lm(ElantraSales~MonthFactor + Unemployment +CPI_all+CPI_energy,data=elantraTrain)
+summary(salesModel4)
+
+salesPred4 = predict(salesModel4,newdata = elantraTest)
+salesPred4
+SSE_sales = sum((salesPred4 - elantraTest$ElantraSales)^2)
+SSE_sales
+
+mean(elantraTrain$ElantraSales)
+
+SST_sales = sum((mean(elantraTrain$ElantraSales)- elantraTest$ElantraSales)^2)
+R2_sales_test = 1 - SSE_sales/SST_sales
+R2_sales_test
+
+max(abs(salesPred4 - elantraTest$ElantraSales))
+
+which.max(abs(salesPred4 - elantraTest$ElantraSales))  #returns 14 5 the fifth row
+elantraTest[5,]
+
+
+#################State Data
+stateData = read.csv("statedata.csv")
+str(stateData)
+head(stateData)
+
+plot(stateData$x, stateData$y)
+
+sort(tapply(stateData$HS.Grad,stateData$state.region,mean))
+
+boxplot( Murder ~state.region, data=stateData)
+tapply(stateData$Murder,stateData$state.region, median )
+
+NEData = subset(stateData, state.region=="Northeast")
+NEData
+which.max(NEData$Murder)
+NEData[6,]
+
+LE.Model1 = lm(Life.Exp ~ Population+ Income+ Illiteracy+ Murder+ HS.Grad+ Frost+ Area, data = stateData )
+summary(LE.Model1)
+
+plot(stateData$Income, stateData$Life.Exp)
+
+LE.Model2 = lm(Life.Exp ~ Population+ Income+ Illiteracy+ Murder+ HS.Grad+ Frost, data = stateData )
+summary(LE.Model2)
+
+LE.Model3 = lm(Life.Exp ~ Population+ Income+ Murder+ HS.Grad+ Frost, data = stateData )
+summary(LE.Model3)
+
+LE.Model4 = lm(Life.Exp ~ Population+ Murder+ HS.Grad+ Frost, data = stateData )
+summary(LE.Model4)
+
+sort(predict(LE.Model4))
+which.min(predict(LE.Model4))
+stateData[1,]
+
+which.min(stateData$Life.Exp)
+stateData[40,]
+
+stateData[which.max(predict(LE.Model4)),]
+stateData[which.max(stateData$Life.Exp),]
+
+sort(abs(LE.Model4$residuals))
+stateData[which.min(abs(LE.Model4$residuals)),]
+stateData[which.max(abs(LE.Model4$residuals)),]
